9ba0c0ee2259fb49789496c80bd9a4dfd71ebb31
Sacha Duvivier <sduvivier@walibuy.com>
2024-11-25 11:56:55 +0100

    Try restarting pipeline

--- app/services/Pokemon.test.ts
+++ app/services/Pokemon.test.ts
@@ -1,5 +1,7 @@
 import { describe, it, expect } from "vitest";
 
+
+// TODO: Write tests for the Pokemon service
 describe("Pokemon", () => {
   it("should_return_true_if_pokemon_is_awesome", () => {
     // Given
